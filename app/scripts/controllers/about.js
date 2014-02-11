3311fa2fd08b98a0e4d79fa4749e4011b3774ada
cktong <chrisktong@gmail.com>
2014-02-10 22:47:14 -0800

    Added backgrounds, routes, fonts

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,4 @@
+'use strict';
+
+angular.module('cktonggithubioApp')
+  .controller('AboutCtrl', function () {});
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,4 +7,13 @@ angular.module('cktonggithubioApp')
       'AngularJS',
       'Karma'
     ];
+
+		var pictures = ['blank','kyoto', 'palace', 'rain'];
+		// var len = pictures.length;
+
+		// for (var i=0; i<len; i++){
+	 //    	$scope.BackgroundClass= pictures[i];
+		// }
+
+		$scope.BackgroundClass=pictures[2];
   });
