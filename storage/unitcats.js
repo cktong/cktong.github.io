1929d96f26ab63874bb3242a5ca41b71c30e35aa
chris <chrisktong@gmail.com>
2017-05-22 13:48:07 -0700

    new color for ny biz map

---
+++ storage/unitcats.js
@@ -0,0 +1,14 @@
+// Define color categories for mapping listings
+// var colors = ['#CC0133', '#990166', '#4D00B2', '#0000FF']; //red-to-blue
+// var colors = ['#f0f9e8', '#bae4bc', '#7bccc4', '#2b8cbe']; //mint
+//var colors = ['#fee5d9', '#fcae91', '#fb6a4a', '#cb181d']; //color brewer red
+// var colors = ['#F80C10', '#F2109B', '#B413ED', '#181AE7']; //red-to-blue2
+// var colors = ['#6EEAF7', '#4B96F0', '#3158EB', '#181AE7']; //red-to-blue
+// var colors = ['#ffffcc','#a1dab4','#41b6c4','#2c7fb8','#253494','#fed976','#feb24c','#fd8d3c','#f03b20','#bd0026']; // Bright 
+// var colors = ['#E1FD3A', '#EA6529', '#F0001F', '#A6071C']; //yellow-to-red
+var colors = ['#bae4bc','#EA6529','#fdffd1']
+
+var nation2 = {0: {'label': '<5', 'color': colors[0]}, 1: {'label': '6&ndash;10', 'color': colors[1]}, 
+			   2: {'label': '11&ndash;40', 'color': colors[2]}};
+
+
